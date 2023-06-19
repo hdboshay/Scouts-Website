2d62500bdca2f0529b916ad7bc06d05a988ee69b
Josh <marshj10@uni.coventry.ac.uk>
2023-06-19 14:31:31 +0100

    removed unused lines

---
+++ public/assets/js/contact.js
@@ -0,0 +1,60 @@
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCdPZxhZTz7gG31tqEZUloEHF1dP-cFEeY",
+  authDomain: "scouts-website-d09ae.firebaseapp.com",
+  databaseURL: "https://scouts-website-d09ae-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "scouts-website-d09ae",
+  storageBucket: "scouts-website-d09ae.appspot.com",
+  messagingSenderId: "129923154201",
+  appId: "1:129923154201:web:e367705fa8d686ebf8c588"
+};
+
+firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  alert("submit")
+  e.preventDefault();
+
+  //Get value
+  var name = document.getElementById('name');
+  var email = document.getElementById('email');
+  var subject = document.getElementById('subject');
+  var message = document.getElementById('message');
+  
+  // Save message
+  saveMessage(name, email, subject, message);
+  
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+  
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+  
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+  
+// Function to get form value
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+  
+  // Save message to firebase
+function saveMessage(name, email, subject, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email: email,
+    subject: subject,
+    message: message
+  });
+}
